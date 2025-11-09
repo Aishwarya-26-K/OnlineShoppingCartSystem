a12cf23a1b4588d10440489042ba5925c7020929
Aishwarya-26-K <aishwaryakarthikeyan26@gmail.com>
2025-11-09 11:37:43 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.myshop.OnlineShoppingCart;
+
+import com.myshop.OnlineShoppingCart.ui.ShopSwingUI;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("🛒 Starting Online Shopping Cart System (GUI)...");
+        // Launch the Swing UI
+        new ShopSwingUI();
+    }
+}
+
+
